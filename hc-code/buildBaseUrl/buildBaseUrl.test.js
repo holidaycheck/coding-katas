2ed81c0f02217bd54ce8c465b3811c669392f949
Jacek Smolak <jacek.smolak@holidaycheck.com>
2018-02-12 11:17:34 +0100

    Work on better test descriptions / variable names / overall readability

--- hc-code/buildBaseUrl/buildBaseUrl.test.js
+++ hc-code/buildBaseUrl/buildBaseUrl.test.js
@@ -2,34 +2,28 @@ import { expect } from 'chai';
 
 import buildBaseUrl from './buildBaseUrl';
 
-describe('buildBaseUrl', function() {
-    it('should be relative to the used protocol', function() {
-        const options = { pathPrefix: '', host: '', isBehindProxy: false };
+describe.only('buildBaseUrl', function() {
+    describe('creates a URL that', () => {
+        const defaultOptions = { pathPrefix: '', host: '', isBehindProxy: false };
 
-        expect(buildBaseUrl(options)).to.startsWith('//');
-    });
+        it('should be relative to the used protocol', function() {
+            expect(buildBaseUrl(defaultOptions)).to.startsWith('//');
+        });
 
-    it('should always end with a trailing slash', function() {
-        const options = { pathPrefix: 'any', host: 'thing', isBehindProxy: false };
+        it('should always end with a trailing slash', function() {
+            expect(buildBaseUrl(defaultOptions)).to.endsWith('/');
+        });
 
-        expect(buildBaseUrl(options)).to.endsWith('/');
-    });
+        it('should concatenate the passed in host and path prefix', function() {
+            const options = { pathPrefix: '/any-path', host: 'example.com', isBehindProxy: false };
 
-    it('should combine the given pathPrefix with the given host', function() {
-        const options = { pathPrefix: 'any-path', host: 'any-host', isBehindProxy: false };
+            expect(buildBaseUrl(options)).to.equal('//example.com/any-path/');
+        });
 
-        expect(buildBaseUrl(options)).to.contain('any-hostany-path');
-    });
+        it('should not contain the pathPrefix if the request is proxied', function() {
+            const options = { pathPrefix: 'any-path', host: 'example.com', isBehindProxy: true };
 
-    it('should build a base url using the passed in host and path prefix', function() {
-        const options = { pathPrefix: 'any-path', host: 'any-host', isBehindProxy: false };
-
-        expect(buildBaseUrl(options)).to.equal('//any-hostany-path/');
-    });
-
-    it('should not contain the pathPrefix if the request is proxied', function() {
-        const options = { pathPrefix: 'any-path', host: 'any-host', isBehindProxy: true };
-
-        expect(buildBaseUrl(options)).to.equal('//any-host/');
+            expect(buildBaseUrl(options)).to.equal('//example.com/');
+        });
     });
 });
